3a0b9419fc4288a4ce7bd68b5f524545b5363c7f
m9-unix <mundusnine@gmail.com>
2020-02-05 20:32:10 -0500

    Update main.xml and khafile.js

--- khafile.js
+++ khafile.js
@@ -1,21 +1,15 @@
 let project = new Project('editor');
 project.addAssets('Assets/**');
-//@Cleanup does nothing but maybe a solution
-//project.addAssets('/home/jsnadeau/foundsdk/haxeui-kha-extended/haxe/ui/extended/_modules/styles/dark/**', { notinlist: true });
 project.addShaders('Shaders/**');
 project.addSources('Sources');
-// project.addSources('editor');
-project.addLibrary('/home/jsnadeau/foundsdk/hscript');
-project.addLibrary('/home/jsnadeau/foundsdk/haxeui-core');
-project.addLibrary('/home/jsnadeau/foundsdk/haxeui-kha');
-project.addLibrary('/home/jsnadeau/foundsdk/haxeui-kha-extended');
+project.addLibrary('Libraries/foundsdk/hscript');
+project.addLibrary('Libraries/foundsdk/haxeui-core');
+project.addLibrary('Libraries/foundsdk/haxeui-kha');
+project.addLibrary('Libraries/foundsdk/haxeui-kha-extended');
+project.addLibrary('Libraries/foundsdk/coineditor');
 project.addDefine("debug");
 project.addDefine("editor");
 project.addDefine("tile_editor");
 project.addParameter("--macro ListTraits.init()")
-// project.addLibrary('/home/jsnadeau/foundsdk/foundry');
-// project.addLibrary('/home/jsnadeau/foundsdk/iron');
-// project.addLibrary('/home/jsnadeau/foundsdk/iron_format');
-// project.addDefine('foundry_editor');
 project.addDefine('editor_dev');
 resolve(project);
