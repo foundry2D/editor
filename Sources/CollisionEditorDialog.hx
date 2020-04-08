9dc82cc61f5ae52271aefc4f2b86941dc6cb9890
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-04-08 12:41:56 -0400

    Added support for Circle shapes and Polygon shapes for collision editor. Right now, I am trying to make the point selection work; we seem to crap ourselves after the first selection...

--- Sources/CollisionEditorDialog.hx
+++ Sources/CollisionEditorDialog.hx
@@ -1,4 +1,6 @@
+
 import kha.Blob;
+import kha.graphics2.GraphicsExtension;
 import khafs.Fs;
 
 import zui.Id;
@@ -11,6 +13,9 @@ import found.anim.Sprite;
 import found.data.SceneFormat;
 
 import echo.data.Types.ShapeType;
+import echo.shape.Rect;
+import echo.shape.Circle;
+import echo.shape.Polygon;
 
 class CollisionEditorDialog {
 	static var textInputHandle:Handle = Id.handle();
@@ -43,7 +48,34 @@ class CollisionEditorDialog {
 			shape = echo.Shape.rect(0,0,image.width,image.height);
 			shapes.push(shape);
 		}
+
 		var selectedCollisionTypeIndex:Int = ui.combo(comboBoxHandle, collisionTypes, "Collision Type");
+
+		if(comboBoxHandle.changed){
+			if(selectedCollisionTypeIndex == ShapeType.RECT){
+				shape = echo.Shape.rect(0,0,image.width,image.height);
+			}
+			if(selectedCollisionTypeIndex == ShapeType.CIRCLE) {
+				var radius = 0.5 * (image.width > image.height ? image.width : image.height);
+				shape = echo.Shape.circle(image.width*0.5,image.height*0.5,radius);
+			}
+			if(selectedCollisionTypeIndex == ShapeType.POLYGON) {
+
+				var verts = [for(i in 0...4)new hxmath.math.Vector2(0,0)];
+				verts[1].x = image.width;
+				verts[2].x = image.width;
+				verts[2].y = image.height;
+				verts[3].y = image.height;
+				shape = Polygon.get_from_vertices(0,0,0,verts);
+			}
+			
+			if(shapes.length > 0){
+				shapes[0] = shape;
+			}
+			else {
+				shapes.push(shape);
+			}
+		}
 				
 		var initY = ui._y;
 		
@@ -60,25 +92,29 @@ class CollisionEditorDialog {
 		var py = ui._y;
 		var state = ui.image(image,0xffffffff,null,0,0,image.width,image.height);
 
-		// var drawX = ui._x;
-		// var drawY = initY;
-		// if(state == State.Down){
-		// 	var x = Math.abs(ui._windowX - ui.inputX) - px;
-		// 	var y = Math.abs(ui._windowY - ui.inputY) - py;
-		// 	trace('x: $x, y: $y');
-
-		// 	drawX = x + ui._x;
-		// 	drawY = y + initY;
-		// 	trace('draw x: $drawX, draw y: $drawY');
-		// }
 		// ui.g.color = kha.Color.Red;
 		// ui.drawRect(ui.g,true,drawX,drawY,10,10);
 		ui._y += ui.ELEMENT_OFFSET() * 2;
 
 		if(shape != null){
+			var color = kha.Color.fromBytes(255,0,0,128);
 			switch(shape.type){
 				case ShapeType.RECT:
-					var rect:echo.shape.Rect = cast(shape);
+					var rect:Rect = cast(shape);
+
+					var xHandle = Id.handle();
+					xHandle.value = rect.x;
+					var x = ui.slider(xHandle,"X",0,sprite.width);
+					if(xHandle.changed){
+						rect.x = x;
+					}
+
+					var yHandle = Id.handle();
+					yHandle.value = rect.y;
+					var y = ui.slider(yHandle,"Y",0,sprite.height);
+					if(yHandle.changed){
+						rect.y = y;
+					}
 
 					var widthHandle = Id.handle();
 					widthHandle.value = rect.width;
@@ -93,26 +129,66 @@ class CollisionEditorDialog {
 					if(heightHandle.changed){
 						rect.height = h;
 					}
-
-					var xHandle = Id.handle();
-					xHandle.value = rect.x;
-					var x = ui.slider(xHandle,"X",0,sprite.width);
-					if(xHandle.changed){
-						rect.x = x;
-					}
-
-					var yHandle = Id.handle();
-					yHandle.value = rect.y;
-					var y = ui.slider(yHandle,"X",0,sprite.height);
-					if(yHandle.changed){
-						rect.y = y;
-					}
-					ui.g.color = kha.Color.fromBytes(255,0,0,128);
+					ui.g.color = color;
 					ui.g.fillRect(ui._x+rect.x,initY+rect.y,rect.width,rect.height);
 					ui.g.color = kha.Color.White;
 				case ShapeType.CIRCLE:
-				case ShapeType.POLYGON:
+					var circle:Circle = cast(shape);
 
+					var xHandle = Id.handle();
+					xHandle.value = circle.x;
+					var x = ui.slider(xHandle,"X",0,sprite.width);
+					if(xHandle.changed){
+						circle.x = x;
+					}
+
+					var yHandle = Id.handle();
+					yHandle.value = circle.y;
+					var y = ui.slider(yHandle,"Y",0,sprite.height);
+					if(yHandle.changed){
+						circle.y = y;
+					}
+
+					var radiusHandle = Id.handle();
+					radiusHandle.value = circle.radius;
+					var maxRadius = 0.5 * (sprite.width < sprite.height ? sprite.width : sprite.height);
+					var radius = ui.slider(radiusHandle,"Radius",1,maxRadius);
+					if(radiusHandle.changed){
+						circle.radius = radius;
+					}
+					ui.g.color = color;
+					GraphicsExtension.fillCircle(ui.g,ui._x+circle.x,initY+circle.y,circle.radius);
+					ui.g.color = kha.Color.White;
+				case ShapeType.POLYGON:
+					var poly:Polygon = cast(shape);
+					ui.text('Number of vertices: '+poly.vertices.length);
+					ui.g.color = color;
+					GraphicsExtension.fillPolygon(ui.g,ui._x+poly.x,initY+poly.y,cast(poly.vertices));
+					var col = kha.Color.fromBytes(0,0,255,128);
+					var selectedCol = kha.Color.fromBytes(0,0,255,255);
+					for(vert in poly.vertices){
+						ui.g.color = col;
+						var w = 10;
+						var addX = ui._x + (vert.x > 0 ? -w : 0.0);
+						var addY = initY + (vert.y > 0 ? -w : 0.0);
+						if(state == State.Down){
+							var x = Math.abs(ui._windowX - ui.inputX) - px ;
+							var y = Math.abs(ui._windowY - ui.inputY) - py ;
+							var tempX = addX- ui._x;
+							var tempY = addY -initY;
+							// selectX = x + ui._x;
+							// selectY = y + initY;
+							trace("Vert X: "+ (vert.x + tempX)+"VertY: " + (vert.y + tempY ));
+							trace('X: $x Y: $y');
+							if(x >= vert.x + tempX &&  x <= vert.x + tempX + w && y >= vert.y + tempY && y <= vert.y + tempY + w){
+								ui.g.color = selectedCol;
+								vert.x = Math.max(0,Math.min(x,image.width));
+								vert.y = Math.max(0,Math.min(y,image.height));
+							} 
+						}
+						ui.g.fillRect( vert.x + addX,vert.y + addY ,w,w);
+					}
+					ui.g.color = kha.Color.White;
 			}
 		}
 
