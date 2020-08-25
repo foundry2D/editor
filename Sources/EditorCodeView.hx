1c86f0ec2792de76f3c490c2cabe8d1ede54054e
Bernard Lambert <bernardlambert2012@gmail.com>
2020-08-25 15:14:02 -0400

    Fixed crash when trying to load internal script + clean up how display is updated when a trait is selected.

--- Sources/EditorCodeView.hx
+++ Sources/EditorCodeView.hx
@@ -8,7 +8,6 @@ import found.data.SceneFormat;
 import found.tool.NodeEditor;
 
 class EditorCodeView implements EditorHierarchyObserver extends Tab {
-
 	var visualEditor:found.tool.NodeEditor;
 
 	var lastDisplayedTrait:TTrait = null;
@@ -19,7 +18,6 @@ class EditorCodeView implements EditorHierarchyObserver extends Tab {
 	var codeScriptTextAreaHandle = Id.handle();
 
 	public function new() {
-
 		EditorHierarchy.register(this);
 	}
 
@@ -27,31 +25,27 @@ class EditorCodeView implements EditorHierarchyObserver extends Tab {
 		// @TODO: should we always load and reparse the data ?
 		// (i.e. we already parse the data to determine what to show the UI so maybe we should load if we find a visual script from the UI)
 		var traits:Array<TTrait> = selectedObject.traits != null ? selectedObject.traits : [];
-		if(traits.length > 0)
+		if (traits.length > 0)
 			setDisplayedTrait(traits[0]);
 	}
 
-	public function setDisplayedTrait(trait:TTrait):Void {		
+	public function setDisplayedTrait(trait:TTrait):Void {
 		currentlyDisplayedTrait = trait;
-		traitNameWindowHandle.redraws = codeScriptWindowHandle.redraws = codeScriptTextAreaHandle.redraws = 2;
-		visualEditor.redraw();
-		if(currentlyDisplayedTrait.type != "VisualScript"){
-			visualEditor.visible = false;
-		}
-		else {
-			visualEditor.visible = true;
+		if (this.active) {
+			traitNameWindowHandle.redraws = codeScriptWindowHandle.redraws = codeScriptTextAreaHandle.redraws = 2;
+			visualEditor.redraw();
 		}
 	}
 
 	@:access(zui.Zui)
 	override public function render(ui:zui.Zui) {
-		if(visualEditor == null){
-			visualEditor = new found.tool.NodeEditor(ui,parent.x, parent.y, parent.w, parent.h);
+		if (visualEditor == null) {
+			visualEditor = new found.tool.NodeEditor(ui, parent.x, parent.y, parent.w, parent.h);
 			visualEditor.visible = false;
 			parent.postRenders.push(visualEditor.render);
 		}
-		visualEditor.setAll(parent.x, parent.y + (ui.t.BUTTON_H + ui.t.ELEMENT_OFFSET)*2, parent.w, parent.h - (ui.t.BUTTON_H + ui.t.ELEMENT_OFFSET)*2);
-		var isActive = ui.tab(parent.htab,"Code");
+		visualEditor.setAll(parent.x, parent.y + (ui.t.BUTTON_H + ui.t.ELEMENT_OFFSET) * 2, parent.w, parent.h - (ui.t.BUTTON_H + ui.t.ELEMENT_OFFSET) * 2);
+		var isActive = ui.tab(parent.htab, "Code");
 		if (isActive) {
 			if (currentlyDisplayedTrait != null) {
 				ui.row([0.7, 0.3]);
@@ -60,18 +54,17 @@ class EditorCodeView implements EditorHierarchyObserver extends Tab {
 					saveDisplayedTraitData();
 				}
 				updateDisplayedTraitData();
-				if(currentlyDisplayedTrait.type != "VisualScript"){
+				if (currentlyDisplayedTrait.type != "VisualScript") {
+					visualEditor.visible = false;
 					var isEditable:Bool = StringTools.endsWith(currentlyDisplayedTrait.classname, ".hx");
 					Ext.textArea(ui, codeScriptTextAreaHandle, zui.Zui.Align.Left, isEditable);
-				}
-				else {
-					updateDisplayedTraitData();
+				} else {
+					visualEditor.visible = true;
 				}
 			}
+		} else {
+			visualEditor.visible = false;
 		}
-		if(!this.active && visualEditor.visible)
-			visualEditor.visible = this.active;
-		
 	}
 
 	function updateDisplayedTraitData() {
@@ -84,9 +77,13 @@ class EditorCodeView implements EditorHierarchyObserver extends Tab {
 						found.tool.NodeEditor.selectedNode = visualTraitData;
 					});
 				} else {
-					khafs.Fs.getContent(currentlyDisplayedTrait.classname, function(data:String) {
-						codeScriptTextAreaHandle.text = data;
-					});
+					if (StringTools.startsWith(currentlyDisplayedTrait.classname, "found.trait.internal")) {
+						trace("need to load internal trait");
+					} else {
+						khafs.Fs.getContent(currentlyDisplayedTrait.classname, function(data:String) {
+							codeScriptTextAreaHandle.text = data;
+						});
+					}
 				}
 			} else {
 				found.tool.NodeEditor.selectedNode = null;
