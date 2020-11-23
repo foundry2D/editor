8f78ec85cdc0fcd49c9834beb07c605b63e5fe47
Jean-Sébastien Nadeau <mundusnine@gmail.com>
2020-11-23 18:20:55 -0500

    Fix play button being uber tiny.

--- Sources/EditorMenuBar.hx
+++ Sources/EditorMenuBar.hx
@@ -1,5 +1,6 @@
 package;
 
+import kha.Image;
 import found.audio.Music;
 import zui.Canvas.TElement;
 import found.App;
@@ -13,6 +14,7 @@ class EditorMenuBar implements View {
 
 	public static inline var defaultMenubarW = 330;
 
+	var ui:zui.Zui;
 	public var workspaceHandle = new Handle({layout: Horizontal});
 	public var menuHandle = new Handle({layout: Horizontal});
     public var menubarw = defaultMenubarW;
@@ -20,17 +22,48 @@ class EditorMenuBar implements View {
 	var playImage:kha.Image;
 	var pauseImage:kha.Image;
 	public function new() {
-		playImage = kha.Assets.images.play;
-		pauseImage = kha.Assets.images.pause;
-	}
 
+	}
+	function shouldRedraw(image:kha.Image,width:Float,height:Float){
+		var should = image == null;
+		should = should ? should : image.width != width || image.height != height;
+		return should;
+
+	}
+	function redrawPlay(size:Float,color:kha.Color){
+		ui.g.end();
+		playImage = Image.createRenderTarget(Std.int(size),Std.int(size));
+		playImage.g2.begin(true,kha.Color.Transparent);
+		playImage.g2.color = color;
+		playImage.g2.fillTriangle(0,0,0,size,size,size*0.5);
+		playImage.g2.end();
+		ui.g.begin(false);
+	}
+	function redrawPause(size:Float,color:kha.Color){
+		ui.g.end();
+		pauseImage = Image.createRenderTarget(Std.int(size),Std.int(size));
+		pauseImage.g2.begin(true,kha.Color.Transparent);
+		pauseImage.g2.color = color;
+		pauseImage.g2.fillRect(0,0,size * 0.15,size);
+		pauseImage.g2.fillRect(size * 0.5,0,size * 0.15,size);
+		pauseImage.g2.end();
+		ui.g.begin(false);
+	}
 	@:access(zui.Zui)
 	public function render(ui:Zui,element:TElement) {
-
+		
+		this.ui = ui;
 		ui.inputEnabled = true;
 		var WINDOW_BG_COL = ui.t.WINDOW_BG_COL;
 		ui.t.WINDOW_BG_COL = ui.t.SEPARATOR_COL;
 		if (ui.window(menuHandle, Std.int(element.x), Std.int(element.y), Std.int(element.width),Std.int(element.height))) {
+			var w = ui.BUTTON_H() > element.height ? element.height: ui.BUTTON_H();
+			if(shouldRedraw(playImage,w,w)){
+				redrawPlay(w,ui.t.ACCENT_COL);
+			}
+			if(shouldRedraw(pauseImage,w,w)){
+				redrawPause(w,ui.t.ACCENT_COL);
+			}
 			var _w = ui._w;
 			ui._x += 1; // Prevent "File" button highlight on startup
 
@@ -65,7 +98,7 @@ class EditorMenuBar implements View {
 			ui._w = _w;
 			ui._x = element.width * 0.5;
 			var origY = ui._y;
-			ui._y =  element.height * 0.5 - playImage.height * 0.25;
+			ui._y =  element.height * 0.1;
 			var currentImage = App.editorui.isPlayMode ? pauseImage : playImage;
 			var state = ui.image(currentImage);
 			if(state == zui.Zui.State.Released){
